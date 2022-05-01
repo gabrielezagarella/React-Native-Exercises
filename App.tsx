cc6a7e1d247767613acb03d2e89ddfe9c958918c
gabrielezagarella <gabry-zaga20@hotmail.it>
2022-05-01 02:24:20 +0200

    feat(exercise-4): add exercise-4

--- App.tsx
+++ App.tsx
@@ -2,39 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  const FullBox = (item: number[]) => {
-    return item.map(() => (
-      <View style={styles.box}>
-        <View style={styles.fullBox} />
-      </View>
-    ));
-  };
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.containerBox}>{FullBox([1, 2])}</View>
-      <View style={{ flex: 1 }}>
-        <View style={{ alignItems: "flex-end" }}>
-          <View style={styles.box} />
-        </View>
-        <View style={{ alignItems: "center" }}>
-          <View
-            style={[
-              styles.box,
-              {
-                backgroundColor: "pink",
-              },
-            ]}
-          />
-        </View>
-        <View style={{ justifyContent: "center" }}>
-          <View style={styles.box} />
-        </View>
-      </View>
-      <View style={styles.containerBottom}>
-        <View style={{ flexDirection: "row" }}>{FullBox([1, 2])}</View>
+      <View style={styles.containerBox}>
+        <View style={styles.rettangle} />
         <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.circleOne} />
+        <View style={styles.circleTwo} />
       </View>
     </View>
   );
@@ -44,32 +20,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    margin: 20,
+    padding: 40,
+    justifyContent: "center",
   },
   containerBox: {
+    borderColor: "grey",
+    borderWidth: 3,
+    height: 200,
+    justifyContent: "space-around",
+    alignItems: "center",
     flexDirection: "row",
-    justifyContent: "flex-end",
-    flex: 1,
-    marginTop: 20,
+    paddingVertical: 20,
+    borderRadius: 10,
   },
-  fullBox: {
-    height: 30,
-    width: 30,
-    backgroundColor: "yellow",
-    borderRadius: 30,
-  },
-  containerBottom: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-end",
-    flex: 1,
+  rettangle: {
+    backgroundColor: "black",
+    height: "100%",
+    width: 80,
   },
   box: {
+    backgroundColor: "red",
+    height: 50,
+    width: 50,
+  },
+  circleOne: {
+    backgroundColor: "pink",
     height: 60,
     width: 60,
-    backgroundColor: "red",
-    margin: 10,
-    justifyContent: "center",
-    alignItems: "center",
+    position: "absolute",
+    bottom: 170,
+    left: 130,
+    borderRadius: 30,
+  },
+  circleTwo: {
+    backgroundColor: "pink",
+    height: 60,
+    width: 60,
+    position: "absolute",
+    top: 170,
+    left: 270,
+    borderRadius: 30,
   },
 });
